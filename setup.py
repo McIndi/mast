d0a060a6934a3f545e956067a823ee166b3d0391
Clifford Bressette <cliffordbressette@mcindi.com>
2019-04-22 13:01:58 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(fname):
 
 setup(
     name="mast",
-    version="2.4.1",
+    version="2.4.2",
     author="Clifford Bressette",
     author_email="cliffordbressette@mcindi.com",
     description=(
